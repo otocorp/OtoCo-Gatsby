bd1b068f573d8ee2387886edaa5fd42f140cbc69
Filipe Soccol <filipe.soccol@gmail.com>
2021-06-04 14:01:24 -0300

    Trying to revert Gatsby version

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,16 +11,14 @@ exports.onCreateWebpackConfig = ({ actions, plugins }) => {
       }),
     ],
     resolve: {
-      fallback: {
-        assert: require.resolve('assert'),
-        stream: require.resolve('stream-browserify'),
-        crypto: require.resolve('crypto-browserify'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        os: require.resolve('os-browserify'),
-        // Buffer: require.resolve('buffer'),
-        // process: require.resolve('buffer'),
-      },
+      // fallback: {
+      //   assert: require.resolve('assert'),
+      //   stream: require.resolve('stream-browserify'),
+      //   crypto: require.resolve('crypto-browserify'),
+      //   http: require.resolve('stream-http'),
+      //   https: require.resolve('https-browserify'),
+      //   os: require.resolve('os-browserify'),
+      // },
       alias: {
         components: path.resolve(__dirname, 'src/components'),
         scss: path.resolve(__dirname, 'src/scss'),
